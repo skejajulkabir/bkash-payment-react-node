1d4deb80d69c3b5a8d6a0815063557a1dfb26dc4
SK EJAJUL KABIR <522ajaj@gmail.com>
2024-09-03 19:27:49 +0600

    c

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import axios from 'axios'
 
 const Home = () => {
 
     const pay = async () => {
         try {
-            const { data } = await axios.post('http://localhost:5000/api/bkash/payment/create', { amount: 50, orderId: 1 }, { withCredentials: true })
+            const { data } = await axios.post('http://localhost:7000/api/bkash/payment/create', { amount: 50, orderId: 1 }, { withCredentials: true })
             window.location.href = data.bkashURL
         } catch (error) {
             console.log(error.response.data)
